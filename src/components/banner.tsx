b56f5f0704b40607960730e333b437f6d2cd3ae4
alexLahady <alexandrelahady@gmail.com>
2025-03-27 19:43:43 +0100

    affichage du nom quand le token existe fonctionnel

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,12 +1,39 @@
 import { Link } from "react-router";
 import "../styles/banner.scss";
-import Cookie from "./cookie";
-
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 
 
 function Banner() {
-    const cookie = Cookie();
-    
+    const [dataUser, setDataUser] = useState<(number | string)[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true); 
+  
+  useEffect(() => {
+    fetch('http://localhost:3001/pro/pro', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include'
+    })
+    .then(response => response.json())
+    .then(arrayUser =>{
+      setDataUser(arrayUser);
+      setIsLoading(false);
+    })
+  },[]);
+
+  let navUser;
+  let fontAwesome = <FontAwesomeIcon icon={faRightFromBracket} />
+  
+  if(dataUser.length <= 2){
+    navUser = isLoading ? (<li style={{color : "white"}}>Loading...</li> ) : (<li style={{color : "white"}}>{dataUser[1]} <span>{fontAwesome }</span></li>)
+  } else {
+    navUser = <li><Link to="/user">Login/Signup</Link></li>
+  }
+  //console.log(dataUser.length <= 2);
+
 
     return (
         <nav className="nav-banner">
@@ -14,7 +41,7 @@ function Banner() {
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/me/movies">Library</Link></li>
                 <li><Link to="/browser">Browser</Link></li>
-                <li> <Link to="/user">Login/Signup</Link></li>
+                {navUser}
             </ul>
         </nav>
     )
